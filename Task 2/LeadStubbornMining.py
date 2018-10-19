24429004dc6eb1df5c51d858327afb980cb8690b
diba-m <diba.m.88@gmail.com>
2018-10-19 14:39:56 +0200

    edited

--- Task 2/LeadStubbornMining.py
+++ Task 2/LeadStubbornMining.py
@@ -8,19 +8,21 @@ class pool:
         self.stubborn_orphans = stubborn_orphans
         self.honest_orphans = honest_orphans
         self.unpublished_blocks = unpublished_blocks
-        
+
 def SimulateLeadStubborn(alpha, gamma, iteration):
     P = pool(0, 0, 0, 0, 0, 0, 0)
-    state = 0    
-    
+    state = 0
+
     for i in range(1,iteration):
         r = random.random()
-        if state == 0: #stubborn miner mined a block
-            if r<alpha:
+        if state == 0:
+            if r<alpha: #stubborn miner mined a block
                 state = 1
                 P.private_chain += 1
                 P.unpublished_blocks += 1
-        
+            else:
+                P.honest_blocks += 1
+
         elif state == 1:
             if r<alpha:
                 state = 2
@@ -110,30 +112,27 @@ def SimulateLeadStubborn(alpha, gamma, iteration):
                         P.stubborn_blocks += 1 #???
                         P.honest_blocks += 1 #???
     return P
-		
+
 def main():
     alpha = 0.33
     gamma = 0.3
     iteration = 10000
     P = SimulateLeadStubborn(alpha, gamma, iteration)
-    
+
     print("\n Iterationations | %d \n α | %f (Selfish miner hash power) \n γ | %f (Proportion of honest miners which mine on the selfish pool)" % (iteration, alpha, gamma))
     #print("\n Theoretical Performance | %f \n Simulated Performance | %f" % ((alpha*(1-alpha)**2*(4*alpha+gamma*(1-2*alpha))-alpha**3)/(1-alpha*(1+(2-alpha)*alpha)), P.stubborn_blocks / float(P.stubborn_blocks + P.honest_blocks)))
     #print("\n Selfish Blocks Mined | %d \n Honest Blocks Mined | %d \n (Selfish Blocks Mined)/(Total Blocks Mined) | %f" % (P.stubborn_blocks, P.honest_blocks, P.stubborn_blocks / float(P.stubborn_blocks + P.honest_blocks)))
     #print("\n Selfish Orphan Blocks | %d \n Honest Orphan Blocks |  %d" % (P.selfish_orphans, P.honest_orphans))
     print(" Difficulty | %d percent" % (float(P.stubborn_blocks + P.honest_blocks)/iteration*100))
-    
+
     print(P.stubborn_blocks)
     print(P.honest_blocks)
     print(P.private_chain)
     print(P.unpublished_blocks)
     print(P.honest_orphans)
     print(P.stubborn_orphans)
-    
-    
-    
+
+
+
 if __name__ == "__main__":
     main()
-				
-		
-				
